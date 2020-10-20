991407ef1d40a566d6793629a62f1765ad03cbae
IldarZar <63116956+IldarZar@users.noreply.github.com>
2020-10-20 15:59:52 +0500

    Добавил переменные, кодировку

--- connect.php
+++ connect.php
@@ -1,6 +1,12 @@
 <?php
+    $db_host = 'localhost';
+    $db_name = 'forum';
+    $db_user = 'root';
+    $db_password = 'root';
 
-    $connect = mysqli_connect('localhost', 'root', 'root', 'forum');
+    $connect = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+    mysqli_set_charset($connect, "utf-8");
 
     if (!$connect){
         print_r("Error");
